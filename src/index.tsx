caf0a971aa70dc4eef1dda13b449d124df34fe6b
roland.pakai <roland.pakai@lightware.com>
2023-11-05 17:10:24 +0100

    Add import flowbite

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'flowbite';
 import flagsmith from 'flagsmith'
 import { FlagsmithProvider } from 'flagsmith/react'
 import './index.css';
