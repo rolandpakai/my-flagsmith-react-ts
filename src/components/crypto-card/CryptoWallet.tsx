98b7a888f37f4f51baf1d2c452e3eb4d448a8304
roland.pakai <roland.pakai@lightware.com>
2023-11-05 20:58:30 +0100

    Change a to div

--- src/components/crypto-card/CryptoWallet.tsx
+++ src/components/crypto-card/CryptoWallet.tsx
@@ -6,10 +6,10 @@ export type CryptoWalletType = {
 const CryptoWallet: React.FC<CryptoWalletType> = ({ name, svg }) => {
 
   return (
-    <a href="#" className="flex items-center p-3 text-base font-bold text-gray-900 rounded-lg bg-gray-50 hover:bg-gray-100 group hover:shadow dark:bg-gray-600 dark:hover:bg-gray-500 dark:text-white">
+    <div className="flex items-center p-3 text-base font-bold text-gray-900 rounded-lg bg-gray-50 hover:bg-gray-100 group hover:shadow dark:bg-gray-600 dark:hover:bg-gray-500 dark:text-white hover:cursor-pointer">
       { svg }
       <span className="flex-1 ml-3 whitespace-nowrap">{ name }</span>
-    </a>
+    </div>
   );
 };
 
